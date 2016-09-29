67e6d7197f32e6f7f89d46460c7c1c0df94992a9
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2016-09-29 13:18:00 +0100

    add scopes to google provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], { name:   "google", scope:  "email,profile", prompt: "select_account", skip_jwt: true }
+
 end
