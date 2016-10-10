927598c2cab2d5af60ad2a6f47a26279cd7378ce
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2016-10-10 14:54:28 +0100

    changed HOSTNAME by HOST

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Dkprovider
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     # set the hostname to send emails
-    config.action_mailer.default_url_options = { :host =>  ENV['HOSTNAME'] }
+    config.action_mailer.default_url_options = { :host =>  ENV['HOST'] }
   end
 end
