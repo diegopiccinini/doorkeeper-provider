7943531c5fb8d3cab127f6c3d1cff622f4bcce05
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2016-10-14 10:28:26 +0100

    assets debug true in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,12 +24,12 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = true
 
+  config.assets.debug = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
