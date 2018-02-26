97b7388b8f7cc9fb6a9b48a52f999296f3104fcf
Diego Hernan Piccinini Lagos <dlagos@bookingbug.com>
2018-02-26 17:57:30 +0000

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # this user could has access to one or more applications
-user = User.create!(email: 'username@example.com', password: 'password', password_confirmation: 'password')
+user = User.find_or_create_by(email: 'username@example.com', password: 'password', password_confirmation: 'password')
+# ApplicationEnvironment sample
+application_environment=ApplicationEnvironment.find_or_create_by name: 'Development'
 # Sample application add access to the user
-user.oauth_applications.create!(name: 'Sample Application', redirect_uri: 'http://localhost:3000/users/auth/doorkeeper/callback')
-
+user.oauth_applications.create!( name: 'Sample Application',
+                                 redirect_uri: 'http://localhost:3000/users/auth/doorkeeper/callback',
+                                 application_environment: application_environment,
+                                 external_id: 'localhost')
 # this an admin user to login in active-admin
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.find_or_create_by(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
