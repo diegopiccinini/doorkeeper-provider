15736369e8d40fe09be2adfae6aa600df8a2ee5b
Diego PL <diego.piccinini@gmail.com>
2018-06-21 16:12:56 +0100

    fixed migration if exists before

--- db/migrate/20180405161851_add_application_environment_to_oauth_application.rb
+++ db/migrate/20180405161851_add_application_environment_to_oauth_application.rb
@@ -1,7 +1,11 @@
 class AddApplicationEnvironmentToOauthApplication < ActiveRecord::Migration
   def change
-    app_env= ApplicationEnvironment.find_or_create_by name: 'Unknown'
-    add_reference :oauth_applications, :application_environment, index: true, default: app_env.id
-    add_foreign_key :oauth_applications, :application_environments
+
+    app_env= ApplicationEnvironment.find_by name: 'Unknown'
+    unless app_env
+      app_env= ApplicationEnvironment.create name: 'Unknown'
+      add_reference :oauth_applications, :application_environment, index: true, default: app_env.id
+      add_foreign_key :oauth_applications, :application_environments
+    end
   end
 end
