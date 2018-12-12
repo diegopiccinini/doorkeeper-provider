2d387eb6456a9fcc9a680038890396a6cbefba99
Diego Piccinini Lagos <dlagos@bookingbug.com>
2018-12-12 12:57:01 +0000

    added Guardfile and check sites status job passed

--- app/jobs/check_sites_status_job.rb
+++ app/jobs/check_sites_status_job.rb
@@ -2,7 +2,12 @@ class CheckSitesStatusJob
   include SuckerPunch::Job
 
   def perform oauth_application_id
-    raise NotImplementedError
+    ActiveRecord::Base.connection_pool.with_connection do
+      app=OauthApplication.find(oauth_application_id)
+      app.sites.each do  |site|
+        site.check
+      end
+    end
   end
 
 end
--- test/jobs/check_sites_status_job_test.rb
+++ test/jobs/check_sites_status_job_test.rb
@@ -6,12 +6,23 @@ class CheckSitesStatusJobTest < ActiveSupport::TestCase
   setup do
     @app=oauth_applications(:one)
     @site=sites(:one)
-    @job=CheckSitesStatusJob.perform_async app.id
+    stub_request(:get, "https://test1.bookingbug.com/logins/auth/bookingbug").
+      with(  headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v0.12.2'
+    }).
+    to_return(status: 302, body: "", headers: {})
+
   end
 
   test "new sites change status" do
     app.sites << site
     association=OauthApplicationsSite.find_by site: site, oauth_application: app
-    assert association.status,'new site'
+    assert_nil association.status
+    assert site.status , 1
+    @job=CheckSitesStatusJob.perform_async app.id
+    site.reload
+    assert site.status , 302
   end
 end
