b0c17b85f5ca1d8325fac3cc1bc77be7ac0cbb64
Diego Piccinini Lagos <dlagos@bookingbug.com>
2019-01-21 11:07:42 +0000

    added filter to user sites

--- app/admin/user_sites.rb
+++ app/admin/user_sites.rb
@@ -12,6 +12,21 @@ ActiveAdmin.register_page "User Sites" do
     redirect_to admin_user_sites_path
   end
 
+  page_action :assigned, method: :get do
+    session[:user_sites_assigned]='assigned'
+    redirect_to admin_user_sites_path
+  end
+
+  page_action :unassigned, method: :get do
+    session[:user_sites_assigned]='unassigned'
+    redirect_to admin_user_sites_path
+  end
+
+  page_action :all, method: :get do
+    session[:user_sites_assigned]='all'
+    redirect_to admin_user_sites_path
+  end
+
   page_action :add, method: :get do
     site=Site.find params[:site_id]
     user=User.find session[:user_id]
@@ -27,16 +42,31 @@ ActiveAdmin.register_page "User Sites" do
   end
 
   content do
+    session[:user_sites_assigned]='all' unless session.has_key?(:user_sites_assigned)
     session[:user_id] = params[:user_id] if params[:user_id]
     session[:user_id]=User.order(:name).first.id unless session[:user_id]
-    sites=Site
+
+    user=User.find(session[:user_id])
+
+    if session[:user_sites_assigned]=='all'
+      sites=Site
+    elsif session[:user_sites_assigned]=='assigned'
+      sites=user.sites
+    else
+      sites=Site.where.not(id: user.sites.ids)
+    end
 
     sites=sites.url_contains(session[:site_name_filter]) if session[:site_name_filter]
 
     sites=sites.order(:url).limit(30)
-    user=User.find(session[:user_id])
     render partial: 'index', locals: { user: user, sites: sites }
   end
 
   sidebar :filters, partial: 'filters'
+end
+
+def style_button assigned
+  button='warning'
+  button='success' if session[:user_sites_assigned]==assigned
+  "c-button c-button--#{button} c-button--block"
 end
