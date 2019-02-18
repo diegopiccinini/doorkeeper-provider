cb113b31dcabbbfcdb63a01082cd89d0ce1eaf90
Diego Piccinini Lagos <dlagos@bookingbug.com>
2019-02-18 16:02:42 +0000

    testing signout

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -41,3 +41,10 @@ function login(id_token) {
 
   }
 }
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
